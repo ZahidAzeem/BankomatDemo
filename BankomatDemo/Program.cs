6ce13558140dff1ba655814b7d218c8328435e50
ZahidAzeem <azeemior@gmail.com>
2021-10-04 11:23:46 +0200

    Add project files.

---
+++ BankomatDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankomatDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
